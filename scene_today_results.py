56c2849bba9f601a8f7fcd6cd111b0f052691423
Anatoli Tjushka <t0l1k@mail.ru>
2021-10-26 16:21:47 +0300

    today scene results in 3 rows

--- scene_today_results.py
+++ scene_today_results.py
@@ -4,25 +4,26 @@ from label import Label
 
 
 class ResultView:
-    def __init__(self, pos, size, boxHeight=50) -> None:
+    def __init__(self, pos, size, boxHeight=50, rows=3) -> None:
         self.pos = pos
         self.image = pygame.Surface(size)
         self.boxHeight = boxHeight
+        self.rows = rows
         self.rect = self.image.get_rect()
         self.createImage()
 
     def createImage(self):
         self.board = pygame.Surface(
-            (self.rect.w, getHeihtForSurface(self.rect.h)[0]))
+            (self.rect.w, getHeihtForSurface(self.rect.h, self.rows)[0]))
         self.board_rect = self.board.get_rect()
         self.board.fill((0, 128, 128))
-        boxWidth = self.rect.w/2
-        boxHeight = getHeihtForSurface(self.rect.h)[1]
+        boxWidth = self.rect.w/self.rows
+        boxHeight = getHeihtForSurface(self.rect.h, self.rows)[1]
         keys = list(conf.todayGamesData.keys())
         values = list(conf.todayGamesData.values())
-        for y in range(len(conf.todayGamesData)//2):
-            for x in range(2):
-                idx = y*2+x
+        for y in range(len(conf.todayGamesData)//self.rows):
+            for x in range(self.rows):
+                idx = y*self.rows+x
                 s = "#{} Уровень:{} Процент:{}".format(
                     keys[idx], values[idx][0], values[idx][5])
                 l = Label(s, (x*boxWidth, y*boxHeight),
@@ -48,9 +49,10 @@ class ResultView:
         screen.blit(self.image, self.pos)
 
 
-def getHeihtForSurface(hH):
-    lenght = len(conf.todayGamesData) if len(conf.todayGamesData)/2 > 0 else 1
-    size = conf.w*0.05
+def getHeihtForSurface(hH, rows):
+    lenght = len(conf.todayGamesData) if len(
+        conf.todayGamesData)/rows > 0 else 1
+    size = conf.w*0.055
     boxHeight = size
     hSurf = boxHeight*lenght
     if boxHeight*lenght < hH:
